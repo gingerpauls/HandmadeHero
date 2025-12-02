c073543e1cad2b4d973b9fb0f4c649b6f7b8803a
hutchinson9736@gmail.com <hutchinson9736@gmail.com>
2025-12-01 23:34:17 -0800

    ep2 35m34s

--- code/handmade.cpp
+++ code/handmade.cpp
@@ -2,12 +2,60 @@
 
 #include <Windows.h>
 
-int WinMain(HINSTANCE hInstance,
-            HINSTANCE hPrevInstance,
-            LPSTR     lpCmdLine,
-            int       nShowCmd)
+WNDPROC Wndproc;
+
+LRESULT MainWindowCallback( HWND WindowHandle,
+                            UINT Message,
+                            WPARAM WParam,
+                            LPARAM LParam)
 {
-    WNDCLASSEXA WindowClass = {0};
+    LRESULT Result = 0;
+
+    switch(Message)
+    {
+        case WM_SIZE:
+        {
+            OutputDebugStringA("WM_SIZE\n");
+        } break;
+
+        case WM_DESTROY:
+        {
+            OutputDebugStringA("WM_DESTROY\n");
+        } break;
+
+        case WM_CLOSE:
+        {
+            OutputDebugStringA("WM_CLOSE\n");
+        } break;
+
+        case WM_ACTIVATEAPP:
+        {
+            OutputDebugStringA("WM_ACTIVATEAPP\n");
+        } break;
+
+        default:
+        {
+            // OutputDebugStringA("DEFAULT\n");
+            Result = DefWindowProc(WindowHandle, Message, WParam, LParam);
+        } break;
+
+        return Result;
+    }
+}
+
+int WinMain(HINSTANCE Instance,
+            HINSTANCE PrevInstance,
+            LPSTR     CommandLine,
+            int       ShowCode)
+{
+    WNDCLASSEXA WindowClass = {};
+    // TODO(paul): check if CS_HREDRAW or CS_VREDRAW still matter
+    WindowClass.style = CS_OWNDC | CS_HREDRAW | CS_VREDRAW;
+    WindowClass.lpfnWndProc = MainWindowCallback;
+    WindowClass.hInstance = Instance;
+    // WindowClass.hInstance = GetModuleHandle(0);
+    // WindowClass.hIcon =  ;
+    WindowClass.lpszClassName = "HandmadeHeroWindowClass";
 
     MessageBoxA(0, "Are you sure?", "Confirm", 
                     MB_YESNO 
